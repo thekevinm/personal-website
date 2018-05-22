26073f3b54232b8b0de3040817d5ab1571bb2405
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-05-22 10:16:43 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/Header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="headerContainer">
+        <header>
+          <h1>Kevin McGahey</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
