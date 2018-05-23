efc3fd6725e585ecc27d2e7a43bf3e9aef72df33
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-05-23 12:11:13 -0700

    added link to each project

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,6 +4,8 @@ import iosApp from '../assets/ezgif.com-video-to-gif.gif'
 import webApp from '../assets/webApp.gif'
 import jsclock from '../assets/gifClock.gif'
 import slacker from '../assets/slackerPic.png'
+import skeu from '../assets/skeucard.gif'
+import git from '../assets/githublogo.svg'
 
 
 var clock = document.getElementById('clock');
@@ -53,23 +55,28 @@ class Info extends Component {
         <h1>Street Eats</h1>
         <img src={webApp} className="webAppContainer"/>
         <p>Street Eats is a single page web app utilizing React and Redux for state management. It is meant for food truck owners to quickly update their location by using geolocation and drop a pin on our map for users to follow their favorite trucks. Users can also order their favorite food by using Stripe for payments and Socket.io for instant order notifications to the food trucks.</p> 
+        <a href="https://github.com/classZero/street-eats" target="_blank"><img src={git} className="gitHubContainer" /></a>
       </div>
 
       <div className="projContainer">
         <h1>Street Eats iOS</h1>
         <img src={iosApp} className="iosAppContainer" />
         <p>Street Eats iOS is the mobile app version that utilizes React Native to create a fun user experience. Trucks can quickly update their locations from their phones and users can instantaneously see where they are.</p>
+        <a href="https://github.com/classZero/street-eats-app" target="_blank"><img src={git} className="gitHubContainer" /></a>
       </div>
 
       <div className="projContainer2">
         <h1>Slack Clone</h1>
         <img src={slacker} className="slackerContainer" />
         <p>Slacker is a Slack clone for instant messaging between users and has to the ability to create custom chat rooms. This app was made possible by using Socket.io for fast instant messaging.</p>
+        <a href="https://github.com/thekevinm/slacker" target="_blank"><img src={git} className="gitHubContainer" /></a>
       </div>
 
       <div className="projContainer2">
         <h1>Credit Card Validator</h1>
-        {/* <img src={iosApp} className="iosAppContainer" /> */}
+        <img src={skeu} className="skeuContainer" />
+        <p>As users enter credit card info, using Regex, it can check for valid information and update the image in the top right to be the correct credit card company.</p>
+        <a href="https://github.com/thekevinm/react-card-validator" target="_blank"><img src={git} className="gitHubContainer" /></a>
       </div>
 
       <div className="projContainer3">
@@ -77,12 +84,8 @@ class Info extends Component {
         <img src={jsclock} className="jsClockContainer" />
         <a className="hex"></a>
         <p>This is a JavaScript clock that uses the current time to correlate to hex colors and is being used right now to change the background color every second!</p>
+        <a href="https://github.com/thekevinm/Javascript-Clock" target="_blank"><img src={git} className="gitHubContainer" /></a>
       </div>
-
-      {/* // <div className="projContainer3">
-      //   <h1>Todo List</h1>
-      //   {/* <img src={iosApp} className="iosAppContainer" /> */}
-      {/* // </div> */}
     
     </div>
 
