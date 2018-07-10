4beb862c8fa742e16f4d5fcd8c3d03140490870c
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-07-10 12:41:10 -0700

    tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Header from './Header'
 import About from './About'
 import Info from './Info'
+import '../styles/Home.css'
 
-class Home extends Component {
+class Home extends Component { 
   render() {
     return (
       <div>
