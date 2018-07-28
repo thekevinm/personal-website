fc61a24a98651bdcdcd61a23c875aa321fdb6a1d
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-07-27 21:00:51 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      // <Router>
         <div>
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} /> */}
+          <Home />
         </div>
-      </Router>
+      // </Router>
     );
   }
 }
